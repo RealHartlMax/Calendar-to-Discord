a4f3cce52f85a982cff1292521dbbf5d846abf38
Florian König-Heidinger <florian@koenig-heidinger.de>
2022-12-23 21:06:29 +0100

    Adding fields

--- Calendar_to_Discord.php
+++ Calendar_to_Discord.php
@@ -14,6 +14,95 @@ function my_plugin_menu() {
     add_options_page( 'Calendar to Discord Options', 'Calendar to Discord', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
 }
 
+// Add Settings Fields
+add_action( 'admin_init',  'rhmc2d_settings_fields' );
+function rhmc2d_settings_fields(){
+
+	// I created variables to make the things clearer
+	$page_slug = 'rhm_c2d';
+	$option_group = 'rhm_c2d_settings';
+    $section_id = 'rhm_c2d_section_id';
+
+	// 1. create section
+	add_settings_section(
+		$section_id, // section ID
+		'', // title (optional)
+		'', // callback function to display the section (optional)
+		$page_slug
+	);
+
+	// 2. register fields
+	register_setting( $option_group, 'rhm_c2d_webhook_url');
+	register_setting( $option_group, 'rhm_c2d_webhook_name');
+	register_setting( $option_group, 'rhm_c2d_webhook_avatar');
+	register_setting( $option_group, 'rhm_c2d_webhook_message');
+
+	// 3. add fields
+	add_settings_field(
+		'rhm_c2d_webhook_url',
+		'WebHook URL',
+		'rhmc2d_field_webhook_url', // function to print the field
+		$page_slug,
+		$section_id // section ID
+	);
+	add_settings_field(
+		'rhm_c2d_webhook_name',
+		'WebHook Name',
+		'rhmc2d_field_webhook_name', // function to print the field
+		$page_slug,
+		$section_id // section ID
+	);
+	add_settings_field(
+		'rhm_c2d_webhook_avatar',
+		'WebHook Avatar',
+		'rhmc2d_field_webhook_avatar', // function to print the field
+		$page_slug,
+		$section_id // section ID
+	);
+	add_settings_field(
+		'rhm_c2d_webhook_message',
+		'WebHook Message',
+		'rhmc2d_field_webhook_message', // function to print the field
+		$page_slug,
+		$section_id // section ID
+	);
+}
+
+// custom callback functions to print field HTML
+function rhmc2d_field_webhook_url( $args ){
+	printf(
+		'<input type="text" id="%s" name="%s" value="%s" />',
+		$args[ 'name' ],
+		$args[ 'name' ],
+		get_option( $args[ 'name' ], '' ) // Second parameter is default value of the field
+	);
+}
+function rhmc2d_field_webhook_name( $args ){
+	printf(
+		'<input type="text" id="%s" name="%s" value="%s" />',
+		$args[ 'name' ],
+		$args[ 'name' ],
+		get_option( $args[ 'name' ], '' ) // Second parameter is default value of the field
+	);
+}
+function rhmc2d_field_webhook_avatar( $args ){
+	printf(
+		'<input type="text" id="%s" name="%s" value="%s" />',
+		$args[ 'name' ],
+		$args[ 'name' ],
+		get_option( $args[ 'name' ], '' ) // Second parameter is default value of the field
+	);
+}
+function rhmc2d_field_webhook_message( $args ){
+	printf(
+		'<input type="text" id="%s" name="%s" value="%s" />',
+		$args[ 'name' ],
+		$args[ 'name' ],
+		get_option( $args[ 'name' ], '' ) // Second parameter is default value of the field
+	);
+}
+
+
 function my_plugin_options() {
     if ( !current_user_can( 'manage_options' ) )  {
         wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
